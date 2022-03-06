4881f2f7e72f7b0a08028e2003d56fa343d9f2e7
Kazuhiko Okumura <engkokmr@gmail.com>
2022-03-07 07:59:13 +0900

    状態によって異なるデザインになるようにコンポーネントを修正した

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -5,14 +5,50 @@ export interface TaskProps {
     state: string;
     updateAt?: Date;
   };
-  onArchiveTask?: () => void;
-  onPinTask?: () => void;
+  onArchiveTask: (id: string) => void;
+  onPinTask: (id: string) => void;
 }
 
-export const Task = (props: TaskProps) => {
+export const Task = ({
+  task: { id, title, state },
+  onArchiveTask,
+  onPinTask,
+}: TaskProps) => {
   return (
-    <div className={"list-item"}>
-      <input type={"text"} value={props.task.title} readOnly={true} />
+    <div className={`list-item ${state}`}>
+      <label className={"checkbox"}>
+        <input
+          type={"checkbox"}
+          defaultChecked={state === "TASK_ARCHIVED"}
+          disabled={true}
+          name={"checked"}
+        />
+        <span
+          className={"checkbox-custom"}
+          onClick={() => onArchiveTask(id)}
+          id={`archiveTask-${id}`}
+          aria-label={`archiveTask-${id}`}
+        />
+      </label>
+      <div className={"title"}>
+        <input
+          type={"text"}
+          value={title}
+          readOnly={true}
+          placeholder={"Input title"}
+        />
+      </div>
+      <div className={"actions"} onClick={(event) => event.stopPropagation()}>
+        {state !== "TASK_ARCHIVED" && (
+          <a onClick={() => onPinTask(id)}>
+            <span
+              className={`icon-star`}
+              id={`pinTask-${id}`}
+              aria-label={`pinTask-${id}`}
+            />
+          </a>
+        )}
+      </div>
     </div>
   );
 };
