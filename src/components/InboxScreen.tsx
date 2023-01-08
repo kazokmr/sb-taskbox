0674dfd1242e33db57b4a7bb80f795d909fa96ea
kazokmr <engkokmr@gmail.com>
2023-01-09 05:54:09 +0900

    chromaticが動くように色々更新した

--- src/components/InboxScreen.tsx
+++ src/components/InboxScreen.tsx
@@ -1,36 +1,35 @@
-import { useDispatch, useSelector } from "react-redux";
-import { fetchTasks, RootState } from "../lib/store";
-import { useEffect } from "react";
-import { TaskList } from "./TaskList";
+import {useSelector} from "react-redux";
+import {fetchTasks, RootState} from "../lib/store";
+import {useEffect} from "react";
+import {TaskList} from "./TaskList";
 
 export const InboxScreen = () => {
-  const dispatch = useDispatch();
-  const { error } = useSelector((state: RootState) => state.taskbox);
+    const {error} = useSelector((state: RootState) => state.taskbox);
 
-  useEffect(() => {
-    dispatch(fetchTasks());
-  }, []);
+    useEffect(() => {
+        fetchTasks()
+    }, []);
 
-  if (error) {
+    if (error) {
+        return (
+            <div className={"page lists-show"}>
+                <div className={"wrapper-message"}>
+                    <span className={"icon-face-sad"}/>
+                    <div className={"title-message"}>Oh no!</div>
+                    <div className={"subtitle-message"}>{error}</div>
+                </div>
+            </div>
+        );
+    }
+
     return (
-      <div className={"page lists-show"}>
-        <div className={"wrapper-message"}>
-          <span className={"icon-face-sad"} />
-          <div className={"title-message"}>Oh no!</div>
-          <div className={"subtitle-message"}>{error}</div>
+        <div className={"page lists-show"}>
+            <nav>
+                <h1 className={"title-page"}>
+                    <span className={"title-wrapper"}>Taskbox</span>
+                </h1>
+            </nav>
+            <TaskList/>
         </div>
-      </div>
     );
-  }
-
-  return (
-    <div className={"page lists-show"}>
-      <nav>
-        <h1 className={"title-page"}>
-          <span className={"title-wrapper"}>Taskbox</span>
-        </h1>
-      </nav>
-      <TaskList />
-    </div>
-  );
 };
